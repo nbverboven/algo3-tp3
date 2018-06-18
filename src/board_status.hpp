c0391c014bde6dc2e52e8a6fafb7a775e0c319f5
Andrés Lasso <lassoandres2@gmail.com>
2018-06-17 22:51:54 -0300

    Vuelvo a poner el constructor que utiliza la catedra. Por favor NO ELIMINARLO! 2da vez..

--- src/board_status.hpp
+++ src/board_status.hpp
@@ -47,6 +47,9 @@ struct player_status {
 	player_status() {}
 	player_status(int id) : id(id) {}
 	player_status(int id, double probability) : id(id), probability(probability) {}
+	player_status(int id, int i, int j, bool in_possession)
+		: id(id), i(i), j(j), in_possession(in_possession) {}
+
 	player_status(int id, int i, int j, double probability, bool in_possession)
 		: id(id), i(i), j(j), probability(probability), in_possession(in_possession) {}
 	player_status(const player_status &otro)
@@ -56,8 +59,7 @@ struct player_status {
 		if (pm.player_id == id) {
 			if (pm.move_type == PASE) {
 				in_possession = false;
-			}
-			else {
+			} else {
 				old_position = std::make_pair(i, j);
 				struct move m(MOVES[pm.dir]);
 				i += m.i;
