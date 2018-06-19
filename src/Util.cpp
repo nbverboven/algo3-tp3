fdedd3061f892fe10e66a0bd5eb0406b4d8d831e
Pablo Salinas <salinas.pablom@gmail.com>
2018-06-19 13:04:32 -0300

    Cambio a double el valor de retorno de distance

--- src/Util.cpp
+++ src/Util.cpp
@@ -3,10 +3,10 @@
 /**
  * Calcula la distancia entre dos puntos
  */
-int distance(const std::pair<int,int>& t1, const std::pair<int,int>& t2){
-    int x = pow((std::get<0>(t2) - std::get<0>(t1)),2);
-    int y = pow((std::get<1>(t2) - std::get<1>(t1)),2);
-    int dis = std::sqrt(x + y);
+double distance(const std::pair<int,int>& t1, const std::pair<int,int>& t2){
+    double x = pow((std::get<0>(t2) - std::get<0>(t1)),2);
+    double y = pow((std::get<1>(t2) - std::get<1>(t1)),2);
+    double dis = std::sqrt(x + y);
     return dis;
 }
 
--- src/Util.h
+++ src/Util.h
@@ -7,6 +7,6 @@
 /**
  * Calcula la distancia entre dos puntos
  */
-int distance(const std::pair<int,int>& t1, const std::pair<int,int>& t2);
+double distance(const std::pair<int,int>& t1, const std::pair<int,int>& t2);
 
 #endif
