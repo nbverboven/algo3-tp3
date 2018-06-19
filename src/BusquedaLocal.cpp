b87ccc11434c39351836f98f49866ced2c8a1658
Nicolás Berríos Verboven <nbverboven@gmail.com>
2018-06-19 11:40:34 -0300

    Un poco más de BusquedaLocal

--- src/BusquedaLocal.cpp
+++ src/BusquedaLocal.cpp
@@ -1,53 +1,99 @@
-#include "referee.h"
+#include "Referee.h"
+#include "GreedyPlayer.hpp"
 
-#define PARTIDOS_A_DISPUTAR 10
+#define GAMES_TO_PLAY 20
+#define MAX_NUMBER_OF_SEARCHS 100
 
 int main()
 {
 	std::vector<player> players;
-	std::vector<player> oponents;
+	std::vector<player> opponents;
 
 	for (int i = 0; i < 3; ++i) {
 		players.push_back(player(i, 0.5));
-		oponents.push_back(player(i, 0.5));
+		opponents.push_back(player(i, 0.5));
 	}
 
 	int columns = 10;
 	int rows = 5;
 	int steps = 50;
 
-	/* Puede ser así o puede ser un struct */
-	// std::vector<int> parametros;
-	// parametros <- los pesos de las características a evaluar al puntuar un tablero
-
-	GreedyPlayer GreedyPlayer(columns, rows, steps, IZQUIERDA, players, oponents);
-	static_player static_p(columns, rows, steps, DERECHA, oponents, players);
-
 	/* La idea es guardar los parámetros usados, y cuántos partidos
 	   ganó mi equipo para esos valores */
-	// std::std::vector<std::pair<int, int, int, int, int,..., partidos_ganados>> game_result;
-	// std::std::vector<std::pair<struct_que_guarda_parámetros, partidos_ganados>> game_result;
+	std::vector<std::pair<genome, int>> game_results;
+	genome current_genome(); //Con algunos valores iniciales
 
-	for (int i = 0; i < PARTIDOS_A_DISPUTAR; ++i) {
-		// Voy a asumir que mi equipo es el A
-		// int games_won_by_my_team = 0;
+	// Contadores de partidos ganados
+	int number_of_local_maxima_found = 0;
+	int consecutive_equal_maxima_found = 0;
+	int last_maximum = 0;
 
-		Referee ref(columns, rows, steps, moronic_p, static_p);
-		std::string the_winner = ref.runPlay(A);
-		
-		// Registro cuántos partidos ganó cada uno
-		// if (the_winner == A) {
-		// 	++games_won_by_my_team;
-		// }
-		// else {
-		//  ++games_won_by_oponent;
-		// }
+	/* Si k1, k2,..., kn son los genes, dado un genoma en particular definimos
+	   su vecindad como un entorno de 0,1 alrededor de cada ki, 1<=i<=n */
+	// Mientras no haya llegado a las 200 (!!) búsquedas
+	// o no converja la cantidad de partidos ganados
+	{
+		GreedyPlayer GreedyPlayer(columns, rows, steps, IZQUIERDA, players, opponents, current_genome);
+		// vecindad_del_genoma_actual <- obtenerVecindad(current_genome)
+
+		// Busco en la vecindad
+		// for (elem in vecindad) do
+		{
+			genome test_genome; // Sus valores dependerán del vecino en cuestión
+			GreedyPlayer GreedyPlayer(columns, rows, steps, DERECHA, opponents, players, test_genome);
+	
+			// Voy a asumir que mi equipo es el A
+			int games_won_by_my_team = 0;
+			int games_won_by_opponent = 0;
+	
+			/* Pongo a los dos equipos a jugar una cantidad de partidos y 
+			   registro cuántos ganó cada uno */
+			for (int i = 0; i < GAMES_TO_PLAY; ++i) {
+				Referee ref(columns, rows, steps, moronic_p, static_p);
+				std::string the_winner = ref.runPlay(A);
+				
+				// Registro quién ganó
+				if (the_winner == A) {
+					++games_won_by_my_team;
+				}
+				else {
+					++games_won_by_opponent;
+				}
+			}
+	
+			// Agrego el genoma más ganador a la lista de resultados
+			if (games_won_by_my_team >= games_won_by_opponent) {
+				game_results.push_back(std::make_pair(current_genome, games_won_by_my_team));
+			}
+			else {
+				game_results.push_back(std::make_pair(test_genome, games_won_by_opponent));
+			}
+		}
+		// end for
+		// fin busco en la vecindad
+
+		/* Busco la primera combinación de parámetros que haya ganado la 
+		   mayor cantidad de partidos*/
+		int i = 0;
+		while (game_results[i].second != last_maximum) {
+			++i;
+		}
+		std::pair<genome, int> best_local_result = game_results[i];
+
+		current_genome = best_local_result.first;
+		int max_local_games_won = best_local_result.second;
+
+		// Veo si puedo estar convergiendo en un máximo local
+		if (max_local_games_won == last_maximum) {
+			++consecutive_equal_maxima_found;
+		}
+		// Veo si encontré un nuevo máximo
+		else if (max_local_games_won > last_maximum) {
+			consecutive_equal_maxima_found = 0;
+			last_maximum = max_local_games_won;
+		}
 	}
-
-	/* Busco la combinación de parámetros que haya ganedo la 
-	   mayor cantidad de partidos. Si hay más de una, puedo elegir todas o
-	   quedarme solo con una */
-	// res <- i \in game_result | i.partidos_ganados = max{ e.partidos_ganados \forall e \in game_result } ;
+	// fin mientras
 
 	return 0;
 }
