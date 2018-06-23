d8204e7c62ce24b317fd6aeb07ab3f2ea3c0358a
Nicolás Berríos Verboven <nbverboven@gmail.com>
2018-06-22 23:17:23 -0300

    Rehice un poco el código de la búsqueda local y cambié el main para que tome como parámetro el nombre del archivo donde logear los resultados

--- src/BusquedaLocal.cpp
+++ src/BusquedaLocal.cpp
@@ -4,108 +4,108 @@
 
 #define ROWS 5
 #define COLUMNS 10
-#define STEPS 50
+#define STEPS 100
 
-#define GAMES_TO_PLAY 20
-#define MAX_NUMBER_OF_SEARCHS 25
+#define GAMES_TO_PLAY 60
+#define MAX_NUMBER_OF_SEARCHS 8
 #define CONVERGENCE_CRITERION 7
 
-int main()
+std::random_device _rd;
+std::mt19937 _generator(_rd());
+static std::uniform_real_distribution<double> urd(-1.0,1.0); // random dice
+std::ofstream log_file;
+
+int main(int argc, char const *argv[])
 {
-	std::vector<player> players;
-	std::vector<player> opponents;
-
-	for (int i = 0; i < 3; ++i) {
-		players.push_back(player(i, 0.5));
-		opponents.push_back(player(i, 0.5));
-	}
-
 	genome current_genome;
-	int neighborhoods_visited = 0;
-	int consecutive_equal_maxima_found = 0;
-	int previous_maximum = 0;
-
+    std::vector<player> players;
+    std::vector<player> opponents;
+    for (unsigned int i = 0; i < 3; ++i) {
+        players.push_back(player(i, 0.5));
+        opponents.push_back(player(i, 0.5));
+    }
+	for (int l = 0; l < 9; l++) {
+        current_genome.genic_values[l] = urd(_generator);
+    }
 	std::ofstream log_file;
-	log_file.open("log/local_search.log");
+    std::string dir = "log/";
+    std::string file_name = dir.append(argv[1]);
+	log_file.open(file_name);
 
-	// while (neighborhoods_visited < MAX_NUMBER_OF_SEARCHS && consecutive_equal_maxima_found < CONVERGENCE_CRITERION) {
+    int consecutive_maxima_found = 0;
+    int last_maximum = 0;
+    for (int i = 0; i < MAX_NUMBER_OF_SEARCHS && consecutive_maxima_found < CONVERGENCE_CRITERION; ++i) {
+        std::vector<std::vector<double>> neighborhood = getNeighborhood(current_genome);
+        std::vector<std::pair<genome, game_series_info>> game_results;
+        GreedyPlayer myTeam(COLUMNS, ROWS, STEPS, IZQUIERDA, players, opponents, current_genome);
+        bool runFirstTeamA = true;
 
-		/* La idea es guardar los parámetros usados, y cuántos partidos
-		   ganó mi equipo para esos valores */
-		std::vector<std::pair<genome, int>> game_results;
+        for (const std::vector<double> &neighbor : neighborhood) {
+    		game_series_info results;
+        	genome test_genome(neighbor);
+            GreedyPlayer opponentTeam(COLUMNS, ROWS, STEPS, DERECHA, opponents, players, test_genome);
 
-		/* Obtengo el vecindario del current_genome y lo recorro
-		   exhaustivamente buscando algún genoma que mejore el mío */
-		std::vector<std::vector<double>> current_neighborhood = getNeighborhood(current_genome);
-		for (std::vector<double> &neighbor : current_neighborhood) {
-			// Sus valores dependerán del vecino en cuestión
-			genome test_genome(neighbor);
-			
-			// Voy a asumir que mi equipo es el A
-			int games_won_by_my_team = 0;
-			int games_won_by_opponent = 0;
-			
-			/* Pongo a los dos equipos a jugar una cantidad de partidos y 
-			   registro cuántos ganó cada uno */
-			for (int i = 0; i < GAMES_TO_PLAY; ++i) {
-			    GreedyPlayer my_team(COLUMNS, ROWS, STEPS, IZQUIERDA, players, opponents, current_genome);
-			    GreedyPlayer opponent(COLUMNS, ROWS, STEPS, DERECHA, opponents, players, test_genome);
-				Referee ref(COLUMNS, ROWS, STEPS, my_team, opponent);
-				std::string the_winner = ref.runPlay(A);
-				
-				if (the_winner == A) {
-					++games_won_by_my_team;
-				}
-				else if (the_winner == B) {
-					++games_won_by_opponent;
-				}
-			}
-	
-			// Agrego los genomas más ganadores que el mío a la lista de resultados
-			if (games_won_by_my_team <= games_won_by_opponent) {
-				game_results.push_back(std::make_pair(test_genome, games_won_by_opponent));
-			}
-		}
+            /* Pongo a los dos equipos a jugar una cantidad de partidos y 
+               registro cuántos ganó cada uno. Asumo que soy A */
+            for (unsigned int l=0; l < GAMES_TO_PLAY; l++) {
+                Referee ref(COLUMNS, ROWS, STEPS, myTeam, opponentTeam);
+                std::string the_winner;
 
-		/* Busco la combinación de parámetros que haya ganado la 
-		   mayor cantidad de partidos contra mi equipo */
-		std::pair<genome, int> best_local_result;
-		if (!game_results.empty()) {
-			best_local_result = maximum(game_results);
-		}
-		else {
-			best_local_result = std::make_pair(current_genome, current_neighborhood.size());
-		}
+                if (runFirstTeamA) {
+                    the_winner = ref.runPlay(A);
+                } else {
+                    the_winner = ref.runPlay(B);
+                }
+                runFirstTeamA = !runFirstTeamA;
 
-		log_file << "================================================" << std::endl;
-		log_file << "VECINDARIO: " << neighborhoods_visited << std::endl;
-		log_file << "GENOMA MÁS GANADOR A NIVEL LOCAL: " << std::endl;
-		log_file << best_local_result.first << std::endl;
-		log_file << "PARTIDOS GANADOS: " << best_local_result.second << std::endl;
-		log_file << "================================================" << std::endl;
+                results.games_played += 1;
+                results.points_scored_A += ref.getTeamScore(A);
+                results.points_scored_B += ref.getTeamScore(B);
+                results.games_won_by_A += (the_winner == A) ? 1 : 0;
+                results.games_won_by_B += (the_winner == B) ? 1 : 0;
+                results.tied_games += (the_winner == "NINGUNO") ? 1 : 0;
+            }
 
-		// Si le gané a todos mis vecinos, soy el más mejor :D
-		// if (best_local_result.first.genic_values == current_genome.genic_values) {
-		// 	break;
-		// }
+            // Me quedo con un genoma mejor o igual que el mío
+            if (results.games_won_by_B >= results.games_won_by_A) {
+                game_results.push_back(std::make_pair(test_genome, results));
+            }
+        } // End for vecino
 
-		// Veo si puedo estar convergiendo en un máximo local
-		int max_local_games_won = best_local_result.second;
-		if (max_local_games_won == previous_maximum) {
-			++consecutive_equal_maxima_found;
-		}
-		// Veo si encontré un nuevo máximo
-		else if (max_local_games_won > previous_maximum) {
-			consecutive_equal_maxima_found = 0;
-			previous_maximum = max_local_games_won;
-		}
+        // Imprimo al standard output porque lo estaba corriendo con nohup
+        if (game_results.empty()) {
+            // Nadie es mejor que yo
+            std::cout << "Ahora sé que existe un Dios... Soy yo." << std::endl;
+            std::cout << current_genome << std::endl;
+            break;
+        } else {
+            // Alguien es igual de bueno o mejor que yo
+            std::pair<genome, game_series_info> local_best = maximum(game_results);
+            current_genome = local_best.first;
 
-		current_genome = best_local_result.first;
-		++neighborhoods_visited;
-	// }
+            std::cout << "Nuevo máximo local, bien piola" << std::endl;
+        	std::cout << current_genome << std::endl;
+            std::cout << "genome_fitness {"           << std::endl;
+    	    std::cout << "    games_played: "         << GAMES_TO_PLAY << std::endl;
+    	    std::cout << "    games_won A: "          << local_best.second.games_won_by_A  << std::endl;
+    	    std::cout << "    games_won B: "          << local_best.second.games_won_by_B  << std::endl;
+    	    std::cout << "    games tied: "           << local_best.second.tied_games      << std::endl;
+    	    std::cout << "    goals A: "              << local_best.second.points_scored_A << std::endl;
+    	    std::cout << "    goals B: "              << local_best.second.points_scored_B << std::endl;
+    	    std::cout << "}"                          << std::endl;
+    	    std::cout << "--------------------------" << std::endl;
 
-	log_file.close();
+            // Veo si puedo estar convergiendo
+            if (local_best.second.games_won_by_A == last_maximum) {
+                ++consecutive_maxima_found;
+            } else {
+                consecutive_maxima_found = 0;
+                last_maximum = local_best.second.games_won_by_A;
+            }
+        }
+    } // End for vecindario
 
-	return 0;
+    std::cout << "TERMINADO" << std::endl;
+    log_file.close();
+    return 0;
 }
-
